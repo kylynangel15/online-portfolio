30f1095384298cc9d31f521426b588b397b7f426
Kylyn Angel Luterte <kylynangel15@gmail.com>
2020-02-25 00:12:38 +0800

    PORTFOLIO PAGE

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -2,14 +2,26 @@
 
 @section('css')
     <link rel="stylesheet" href="{{ asset(mix('/css/portfolio.css')) }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+
+    <style type="text/css">
+    	.gallery {
+    		display: none;
+    	}	
+    
+		.gallery > a > img {
+			width: 10%;
+		}		
+    </style>
 @endsection
 
 
 @section('main')
 <div class='portfolio'>
 	<div class="portfolio-container">
+		
 		<div class='portfolio-item'>
-			<img src="{{ asset('assets/images/murasaki/murasaki7.png') }}">
+			<img src="{{ asset('assets/images/murasaki/murasaki7.png') }}" onclick="test()">
 			<a href="https://www.murasaki7.com/en" target="_blank">Murasaki7</a>
 		</div>
 		<div class='portfolio-item'>
@@ -31,8 +43,31 @@
 	</div>
 </div>
 
+<div class="gallery">
+	<a id="sample" href="{{ asset('assets/images/murasaki/m7_1.png') }}" data-fancybox="gallery" data-caption="Visit <a href='http://www.murasaki7.com/en' target='_blank'>Murasaki7</a> ">
+		<img src="{{ asset('assets/images/murasaki/m7_1.png') }}" >
+	</a>
+    <a href="{{ asset('assets/images/murasaki/m7_2.png') }}" data-fancybox="gallery">
+  		<img src="{{ asset('assets/images/murasaki/m7_2.png') }}" >
+    </a>
+	<a href="{{ asset('assets/images/murasaki/m7_3.png') }}" data-fancybox="gallery">
+		<img src="{{ asset('assets/images/murasaki/m7_3.png') }}" >
+	</a>
+</div>
+
 @endsection
 
 
 @section('js')
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
+<script type="text/javascript">
+	function test() {
+		$( "#sample" ).click();	 	
+	}
+
+
+
+</script>
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+	.js('resources/js/app.js', 'public/js')
     .js('resources/js/master.js', 'public/js')
+    .js('resources/js/portfolio.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/master.scss', 'public/css')
     .sass('resources/sass/about.scss', 'public/css')
